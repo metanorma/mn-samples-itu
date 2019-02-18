1fcaf5043ec165a24a180f8387b2024dd0faea43
Nick Nicholas <opoudjis@optusnet.com.au>
2019-02-18 20:47:19 +1100

    init

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
-bundle install
+bundle update
 asciidoctor --trace -b itu -r 'metanorma-itu' T-REC-J.1108-201901-P.MSW-E.adoc
 asciidoctor --trace -b itu -r 'metanorma-itu' T-REC-H.782-201811-I.MSW-E.adoc
